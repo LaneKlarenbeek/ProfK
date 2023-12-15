26057017ab58cbe428b73c2d0148b493eb599d26
LaneKlarenbeek <lane.klarenbeek@gmail.com>
2023-12-14 18:49:14 -0600

    THe structure of website navigation is in place.

---
+++ practice.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("") ;
+    document.getElementById("introp").style.color = "red";
+}
+function backFunction(){
+    document.getElementById("introp").style.color = "black";
+}
